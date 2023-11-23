197be1cd349448e366082ba6411a4ff607f952d3
Lorena <lorena.rodrigues@ucsal.edu.br>
2023-11-23 14:26:53 -0300

    Primeiro commit

---
+++ quiz/src/App.jsx
@@ -0,0 +1,24 @@
+//react,componentes, estáticos
+import { useContext } from "react";
+import { QuizContext } from "./context/quiz";
+
+import Welcome from "./components/Welcome";
+import Question from "./components/Question";
+
+import "./App.css";
+
+
+function App() {
+
+  const [quizState, dispatch] = useContext(QuizContext);
+
+  return (
+    <div className ="App">
+      <h1>Quiz de programação</h1>
+     {quizState.gameStage === "Start" && <Welcome/>}
+     {quizState.gameStage === "Playing" && <Question/>}
+    </div>
+  );
+}
+
+export default App;
